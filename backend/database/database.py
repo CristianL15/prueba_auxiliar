55f5dcd3ba440f906cf55899c21f90a98261095a
CristianL <cristian.lizarazoc@udea.edu.co>
2026-05-17 20:00:17 -0500

    Feat: crear sesión para realizar las consultas sql

--- backend/database/database.py
+++ backend/database/database.py
@@ -1,9 +1,21 @@
 import sqlalchemy as sa
+from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-url = os.getenv("DATABASE_URL")
-engine = sa.create_engine(url) # type: ignore
+DATABASE_URL = os.getenv("DATABASE_URL", "")
+debugging = os.getenv("DATABASE_DEBUG", False)
 
+engine = sa.create_engine(DATABASE_URL, echo=debugging)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+def get_db():
+  db = SessionLocal()
+  try:
+    yield db
+  finally:
+    db.close()
