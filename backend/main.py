228a891e968ef01fb22b7b8b08ad734a1d60b754
CristianL <cristian.lizarazoc@udea.edu.co>
2026-05-18 19:36:19 -0500

    feat: crear api con routers, cors, y base de datos al iniciar
    Cambiar la llamada on_event que está deprecada

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from backend.database.database import engine, Base
+from backend.controllers import usuarios, productos, compra
+import uvicorn
 
-app = FastAPI()
+app = FastAPI(title="CRUD API")
 
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=["*"],
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
 
+app.include_router(usuarios.router)
+app.include_router(productos.router)
+app.include_router(compra.router)
+
+@app.on_event("startup")
+def startup():
+  Base.metadata.create_all(bind=engine)
